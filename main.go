e078aad0f88b3239c57659747548fc26308fab82
Lorenzo Palinuro Faletra <palinuro@parrotsec.org>
2021-07-07 12:30:50 +0200

    use goroutines to scan repos in background

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/parrotsec/mirrordirector/files"
@@ -14,12 +15,19 @@ func main() {
 	cache := "cache.db"
 	Root := mirrors.Init(config)
 	Root.Show()
-	Root.Scan()
-	Root.Show()
 
 	Fileset := files.Init(Root, cache)
 	Fileset.ScanMaster(Root)
 	Fileset.SaveCache(cache)
 
+	go func(F *files.Fileset, R *mirrors.Root) {
+		for {
+			R.Scan()
+			R.Show()
+			time.Sleep(time.Minute * 5)
+		}
+	}(&Fileset, &Root)
+
+	fmt.Println("starting server")
 	server.Serve(":8080", Fileset, Root)
 }
